cc1bdc1a2d6946fb71ecd3c2462ad353e4196243
Dan Saunders <dan.saunders@ecotricity.co.uk>
2018-09-18 11:02:09 +0100

    started styling

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -6,24 +6,28 @@ class Courses extends Component {
     const { courses } = this.props;
     return (
       <Fragment>
-        <h1 class="page-header text-center">Dev Course Planner</h1>
-        {Object.values(courses).length ? (
-          <ul className="list-group">
-            {Object.values(courses).map((course, i) => (
-
-              <li className="list-group-item row" id={course.id} key={i}>
-                {/* <Link to={"/courses/" + course.id}>{course.title}</Link> */}
-                <p className="col-xs-3"><strong>Title:</strong> {course.title}</p>
-                <p className="col-xs-5"><strong>Description:</strong> {course.description}</p>
-                <button className="col-xs-1">View detail</button>
-                <button className="col-xs-1 pull-right"><strong>+</strong> Add to list</button>
-
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No courses found</p>
-        )}
+          <h1 className="page-header text-center">Dev Course Planner</h1>
+          {Object.values(courses).length ? (
+            <ul className="list-group">
+              {Object.values(courses).map((course, i) => (
+                <li className="list-group-item row" id={course.id} key={i}>
+                  {/* <Link to={"/courses/" + course.id}>{course.title}</Link> */}
+                  <p className="col-xs-3">
+                    <strong>Title:</strong> {course.title}
+                  </p>
+                  <p className="col-xs-5">
+                    <strong>Description:</strong> {course.description}
+                  </p>
+                  <button className="col-xs-1.5 btn btn-primary">View detail</button>
+                  <button className="col-xs-1.5 pull-right btn btn-success">
+                    <strong>+</strong> Add to list
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No courses found</p>
+          )}
       </Fragment>
     );
   }
