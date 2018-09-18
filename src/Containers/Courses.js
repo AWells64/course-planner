d3ccfe55ff5f1fcc2a5caca9df28ae37d413a76b
Dan Saunders <dan.saunders@ecotricity.co.uk>
2018-09-18 12:21:57 +0100

    added actions and reducer function for courses

--- src/Containers/Courses.js
+++ src/Containers/Courses.js
@@ -1,14 +1,15 @@
 import { connect } from "react-redux";
-import Courses from "../Components/Courses"
+import Courses from "../Components/Courses";
 
 const mapStateToProps = state => {
   return {
-    courses: Object.values(state.courses),
+    courses: Object.values(state.courses)
   };
 };
 
 // const mapDispatchToProps = state => {
 //   return {
+//     addCourse: id => dispatch(addCourse(id))
 //   };
 // };
 
