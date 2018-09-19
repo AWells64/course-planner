6823d22c5986600c90fe4a36caa4b56ec2c95fd1
Dan Saunders <dan.saunders@ecotricity.co.uk>
2018-09-19 11:02:41 +0100

    added links between register and login pages

--- src/Components/Forms/Login.jsx
+++ src/Components/Forms/Login.jsx
@@ -1,125 +1,141 @@
 import React, { Component, Fragment } from "react";
 
 class Login extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			fields: [
-				// {name: "id", value: ""},
-				// {name: "Name", value: ""},
-				{name: "Email:", value: ""},
-				{name: "Password:", value: ""},
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: [
+        // {name: "id", value: ""},
+        // {name: "Name", value: ""},
+        { name: "Email:", value: "" },
+        { name: "Password:", value: "" }
+      ]
+    };
+    this.onChange = this.onChange.bind(this);
+  }
 
-			],
-		};
-		this.onChange = this.onChange.bind(this);
-	}	
+  onChange(e, i) {
+    let fields = this.state.fields.slice();
+    fields[i].value = e.target.value;
+    this.setState({ fields: fields });
+    // console.log(fields)
+  }
 
-	onChange(e, i) {
-		let fields = this.state.fields.slice();
-		fields[i].value = e.target.value;
-		this.setState({fields: fields});
-		// console.log(fields)
-	}
+  onSubmit(e) {
+    e.preventDefault();
 
-	onSubmit(e) {
-		e.preventDefault();
+    let userData = {
+      email: this.state.fields[0].value,
+      password: this.state.fields[0].value
+    };
 
-		let userData = {
-			email: this.state.fields[0].value,
-			password: this.state.fields[0].value,
-		};
+    this.props.loginUser(userData);
 
-		this.props.loginUser(userData);
+    this.setState({
+      email: "",
+      password: ""
+    });
+  }
 
-		this.setState({
-			email: "",
-			password: "",
-		})
-	}
-	
-	render() {
-		return (
-			<Fragment>
-			<h2 style={mainHeader}>Dev Course Planner</h2>
-			<hr style={hrStyle} />
-			<div style={registerBackground}>
-				<h2 style={headerText}>Sign In</h2>
-				<form onSubmit={(e) => this.onSubmit(e)}>
-					{this.state.fields.map((field, i) => (
-						<div style={inputSpacing} key={i}>
-							<label style={labelText}>{field.name}</label>
-							<input style={inputSize}
-								type="text" 
-								value={field.value} 
-								onChange={(e) => this.onChange(e, i)
-							}/>
-						</div>
-					))}
+  render() {
+    return (
+      <Fragment>
+        <h2 style={mainHeader}>Dev Course Planner</h2>
+        <hr style={hrStyle} />
+        <div style={registerBackground}>
+          <h2 style={headerText}>Sign In</h2>
+          <form onSubmit={e => this.onSubmit(e)}>
+            {this.state.fields.map((field, i) => (
+              <div style={inputSpacing} key={i}>
+                <label style={labelText}>{field.name}</label>
+                <input
+                  style={inputSize}
+                  type="text"
+                  value={field.value}
+                  onChange={e => this.onChange(e, i)}
+                />
+              </div>
+            ))}
 
-					<input type="submit" style={submitButton} className="btn btn-danger" value="Sign in" />
-				</form>
-				</div>
-			</Fragment>
-		)
-	}
+            <input
+              type="submit"
+              style={submitButton}
+              className="btn btn-danger"
+              value="Sign in"
+            />
+          </form>
+          <p style={register}>
+            Don't have an account yet? <a href="./register">Register here.</a>
+          </p>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 const mainHeader = {
-	fontFamily: 'Bitter',
-	textAlign: "center",
-	backgroundColor: "black",
-	color: "red",
-	borderRadius: 5,
+  fontFamily: "Bitter",
+  textAlign: "center",
+  backgroundColor: "black",
+  color: "red",
+  borderRadius: 5
 };
 
 const hrStyle = {
-    border: 0,
-    height: 0, /* Firefox... */
-    boxShadow: "0 0 10px 1px black",
-    width: '99%',
+  border: 0,
+  height: 0 /* Firefox... */,
+  boxShadow: `2px 6px 2px black`,
+  width: "99%"
 };
 
 const headerText = {
-    color: "white",
-    marginBottom: 40,
-    textAlign: "center",
-    fontFamily: 'Bitter',
+  color: "white",
+  marginBottom: 40,
+  textAlign: "center",
+  fontFamily: "Bitter"
 };
 
 const labelText = {
-    color: "white",
-    marginTop: 20,
-    fontFamily: 'Bitter',
+  color: "white",
+  marginTop: 20,
+  fontFamily: "Bitter"
 };
 
 const registerBackground = {
-	backgroundColor: "black",
-	width: 500,
-	alignItems: "center",
-	justifyContent: "center",
-	padding: 50,
-	margin: "0 auto 0 auto",
+  backgroundColor: "black",
+  width: 500,
+  alignItems: "center",
+  justifyContent: "center",
+  padding: 30,
+  margin: "0 auto 0 auto",
 	marginBottom: 40,
+	borderRadius: 10,
+	boxShadow: `2px 6px 2px black`,
 };
 
 const inputSize = {
-	width: 400,
+  width: 400
 };
 
 const submitButton = {
-	width: 400,
-	marginTop: 50,
-	fontFamily: 'Bitter',
+  width: 400,
+  marginTop: 50,
+  fontFamily: "Bitter"
 };
 
 const inputSpacing = {
-	display: "block",
-	marginVertical: 10,
+  display: "block",
+  marginVertical: 10
 };
 
 const font = {
-  fontFamily: 'Bitter',
+  fontFamily: "Bitter"
+};
+
+const register = {
+  fontFamily: "Bitter",
+  paddingTop: 30,
+  textAlign: "center"
 };
 
 export default Login;
